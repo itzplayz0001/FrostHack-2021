54a606f1b2ed226abd25023bdd3d3f076813b080
itzplayz0001 <66119667+itzplayz0001@users.noreply.github.com>
2021-05-07 23:15:40 +0530

    Update invites.js

--- Community/invites.js
+++ Community/invites.js
@@ -17,7 +17,10 @@ function createInvite(id, inviteid) {
     }
 }
 
+/*
 function validateInvite(id, inviteid) {
+    console.log(id)
+    console.log(inviteid)
     let obj = JSON.parse(fs.readFileSync("invites.json", "utf-8"))
 
     obj["communities"][id].forEach(element => {
@@ -27,6 +30,22 @@ function validateInvite(id, inviteid) {
             return false;
         }
     });
+}*/
+
+function validateInvite(id, inviteId) {
+    console.log(`Id: ${id} InviteID: ${inviteId}`);
+  
+    let invitesAsJSON = JSON.parse(fs.readFileSync("invites.json", "utf-8"));
+  
+    let validInvites = invitesAsJSON["communities"][id].find(e => e["inviteId"] === inviteId);
+  
+    let isValid = false;
+  
+    if(validInvites !== undefined) {
+      isValid = true;
+    }
+  
+    return isValid;
 }
 
 module.exports = {
